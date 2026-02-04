7af5a2c1feeb03de1c7f9a54eb78e06d8dd7d24d
ahmadqb <ahmadqb@github.com>
2026-02-04 13:16:45 +0300

    creates a new NodeJS app

---
+++ src/test.sh
@@ -0,0 +1,17 @@
+EXPECTED="Hello, Test"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+
+if ["$OUTPUT" == "$EXPECTED"];then
+
+echo "passedTest"
+
+exit 0
+
+else
+
+echo "not pass '$EXPECTED' BUT GOT '$OUTPUT'"
+
+exit 1
+
+fi
